b735c7a114662d02a73838eb498b1f5f056272d1
Anjani B M <anjani.b-m@dxc.com>
2024-02-29 09:26:34 +0530

    Add project files.

---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace mockEcomm
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
